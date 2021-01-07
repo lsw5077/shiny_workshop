f683329ea213d411a02c4c861d7fbffd1b006f89
Lyndsie Wszola <lyndsiewszola5@gmail.com>
2021-01-07 08:07:16 -0600

    Update app.R
    made crappie app crappier

--- exampleApps/CrappieFishApp/app.R
+++ exampleApps/CrappieFishApp/app.R
@@ -7,38 +7,15 @@ ui <- fluidPage(
 
       navbarPage("A very crappie fish app"),
       sidebarLayout(
-               
              sidebarPanel(checkboxGroupInput(inputId = "fishChoices",
                                       label = "Choose some fish",
-                                      choices = c("Walleye",
-                                                  "Lake trout",
-                                                  "Yellow perch",
-                                                  "Northern pike",
-                                                  "Muskellunge",
-                                                  "Burbot",
-                                                  "Lake sturgeon"))),
+                                      choices = c("Walleye","Lake trout","Yellow perch","Northern pike","Muskellunge","Burbot", "Lake sturgeon"))),
                mainPanel(plotOutput("lengthPlot")))
 )
 
 server <- function(input, output) {
-
-  
-  sppList <- data.frame(Species = c("Sander vitreus",
-                                    "Salvelinus namaycush",
-                                    "Perca flavescens",
-                                    "Esox lucius",
-                                    "Esox masquinongy",
-                                    "Lota lota",
-                                    "Acipenser fulvescens"),
-                        Common_Name = c("Walleye",
-                                        "Lake trout",
-                                        "Yellow perch",
-                                        "Northern pike",
-                                        "Muskellunge",
-                                        "Burbot",
-                                        "Lake sturgeon")) 
-  
-
+  sppList <- data.frame(Species = c("Sander vitreus","Salvelinus namaycush","Perca flavescens", "Esox lucius","Esox masquinongy","Lota lota","Acipenser fulvescens"),
+                        Common_Name = c("Walleye","Lake trout","Yellow perch","Northern pike","Muskellunge", "Burbot","Lake sturgeon")) 
   params <-  sppList %>%
               left_join(popgrowth(), by = "Species") %>%
               group_by(Species) %>%
@@ -48,8 +25,6 @@ server <- function(input, output) {
                      tmax = round(mean(tmax, na.rm = TRUE))) %>%
               select(Species, Common_Name, lAsym, k, t0, tmax) %>% 
               distinct()
-  
-  
             Walleye <- data.frame(Age = seq(0:params$tmax[params$Common_Name == "Walleye"]), Common_Name = "Walleye")
             Walleye$Length <- params$lAsym[params$Common_Name == "Walleye"]*(1-exp(-params$k[params$Common_Name == "Walleye"]*(Walleye$Age - params$t0[params$Common_Name == "Walleye"])))
             Perch <- data.frame(Age = seq(0:params$tmax[params$Common_Name == "Yellow perch"]), Common_Name = "Yellow perch")
@@ -64,25 +39,13 @@ server <- function(input, output) {
             Burbot$Length <- params$lAsym[params$Common_Name == "Burbot"]*(1-exp(-params$k[params$Common_Name == "Burbot"]*(Burbot$Age - params$t0[params$Common_Name == "Burbot"])))
             Sturgeon <- data.frame(Age = seq(0:params$tmax[params$Common_Name == "Lake sturgeon"]), Common_Name = "Lake sturgeon")
             Sturgeon$Length <- params$lAsym[params$Common_Name == "Lake sturgeon"]*(1-exp(-params$k[params$Common_Name == "Lake sturgeon"]*(Sturgeon$Age - params$t0[params$Common_Name == "Lake sturgeon"])))
-            
             fish <- do.call(rbind, list(Walleye, Perch, Trout, Pike, Muskellunge, Burbot, Sturgeon)) 
-              
-            
             fishLengths <- reactive({
-                            
                             fishLengths <- fish %>% filter(Common_Name %in% input$fishChoices)
-              
                               })
-            
-
             output$lengthPlot <- renderPlot({
-            
                                  graphFish <- fishLengths()
-            
-                                 ggplot(graphFish) +
-                                 geom_line(aes(x = Age, y = Length, group = Common_Name, color = Common_Name)) +
-                                 theme_bw()
-            
+                                 ggplot(graphFish) +geom_line(aes(x = Age, y = Length, group = Common_Name, color = Common_Name)) +theme_bw()
                                  })
 
 }
